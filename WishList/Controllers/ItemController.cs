ba0a0f8af6a47eac52c11ec4298ad6ff2f9e6e35
wahoo189 <MBogdanski@riafinancial.com>
2019-11-11 22:54:17 -0700

    6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.Where(i => i.Id == id);
+            _context.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
